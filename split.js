18b969e0d8ce3a9401b362445eb603e46a1ad045
june.kim <june.kim@kakaocorp.com>
2018-12-28 11:32:36 +0900

    split

---
+++ split.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+// normal
+assert.deepStrictEqual("hello-world".split("-")[0], "hello");
+assert.deepStrictEqual("hello-world".split("-")[1], "world");
+
+// 3 length separator
+assert.deepStrictEqual("hello-+xworld".split("-+x")[0], "hello");
+assert.deepStrictEqual("hello-+xworld".split("-+x")[1], "world");
+
+// limit
+assert.deepStrictEqual("nice-to-meet-you".split("-", 1)[0], "nice");
+assert.deepStrictEqual("nice-to-meet-you".split("-", 1)[1], undefined);
+
+// out of range - undefined
+assert.deepStrictEqual("hello-world".split("-")[2], undefined);
+
